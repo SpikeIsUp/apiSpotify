cc6ab171c2fd0dc6ad6dcc915331ea254a4af725
Quentin <quentin.guyenot@ynov.com>
2025-11-21 09:37:20 +0100

    creation du server

--- site-web/main.go
+++ site-web/main.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := ":8080"
+	
+	fmt.Printf("Serveur lancé--> http://localhost%v\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
